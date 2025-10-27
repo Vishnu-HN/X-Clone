ac968bdbf78cb90821dbbf6cbfddb90aeffbe1d8
VISHNU23becs185 <vishnungowda30@gmail.com>
2025-10-27 14:57:27 +0530

    post routes and controllers added

---
+++ backend/src/middleware/upload.middleware.js
@@ -0,0 +1,20 @@
+//multer is a middeleware for handeling multipart/form-data, which is primarily used for uploading files
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+});
+
+export default upload;
